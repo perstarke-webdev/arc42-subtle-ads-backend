e5e38de4115b5b4391dfd950cf65c4f97d6b9d61
Per Starke <per@starke-team.de>
2023-12-10 17:41:17 +0100

    try returning some html instead of plain text

--- api/index.py
+++ api/index.py
@@ -1,11 +1,27 @@
 from http.server import BaseHTTPRequestHandler
 
 
-class handler(BaseHTTPRequestHandler):
-
+class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'text/html')  # Change content type to 'text/html'
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+
+        # Write your HTML content here
+        html_content = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>My HTMX App</title>
+        </head>
+        <body>
+            <h1>Hello, world!</h1>
+        </body>
+        </html>
+        """
+
+        self.wfile.write(html_content.encode('utf-8'))
         return
