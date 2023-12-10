c0d4c46020f24bcbecbf0e2fdc7e3845669a7de3
Per Starke <per@starke-team.de>
2023-12-10 18:16:07 +0100

    try solving cors error

--- api/index.py
+++ api/index.py
@@ -4,13 +4,14 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
+        self.send_header('Access-Control-Allow-Origin', '*')
         # Add headers for htmx compatibility
         self.send_header('HX-Request', 'true')
         self.send_header('HX-Trigger', 'true')
         self.send_header('HX-Swap', 'true')
         self.end_headers()
 
-        content = "<h1>HELLOOOOO</h1>"
+        content = "<h1>HELLOOOOO TEST</h1>"
 
         self.wfile.write(content.encode('utf-8'))
 
