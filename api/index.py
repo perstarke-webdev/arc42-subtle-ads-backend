cf5fa8793469b4aa98f9cc22c9e304ff72cdbb55
Per Starke <per@starke-team.de>
2023-12-10 18:21:42 +0100

    try specific local origin instead of *

--- api/index.py
+++ api/index.py
@@ -4,23 +4,22 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        # Add headers for htmx compatibility
+        self.send_header('Access-Control-Allow-Origin', 'http://0.0.0.0:4000')
         self.send_header('HX-Request', 'true')
         self.send_header('HX-Trigger', 'true')
         self.send_header('HX-Swap', 'true')
         self.end_headers()
 
-        content = "<h1>HELLOOOOO TEST</h1>"
-
+        content = "<h1>HELLOOOOO</h1>"
         self.wfile.write(content.encode('utf-8'))
 
     def do_OPTIONS(self):
         self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Origin', 'http://0.0.0.0:4000')
         self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
         self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         self.end_headers()
+
 
 if __name__ == '__main__':
     from http.server import HTTPServer
