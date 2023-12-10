5aa9ce1db24c8ae9293f5cf7a1a8cec75acfd1f5
Per Starke <per@starke-team.de>
2023-12-10 18:04:50 +0100

    try get request compatibility

--- api/index.py
+++ api/index.py
@@ -1,11 +1,13 @@
 from http.server import BaseHTTPRequestHandler
 
-
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
+        # Add headers for htmx compatibility
+        self.send_header('HX-Request', 'true')
+        self.send_header('HX-Trigger', 'true')
+        self.send_header('HX-Swap', 'true')
         self.end_headers()
 
         content = "<h1>HELLOOOOO</h1>"
