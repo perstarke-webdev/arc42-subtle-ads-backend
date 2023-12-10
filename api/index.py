19bf8919be01c1968f81e7b08c21ee86b285027c
Per Starke <per@starke-team.de>
2023-12-10 17:46:12 +0100

    try variable for text

--- api/index.py
+++ api/index.py
@@ -1,26 +1,16 @@
 from http.server import BaseHTTPRequestHandler
 
 
-class MyHandler(BaseHTTPRequestHandler):
+class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-        # Write your HTML content here
-        html_content = """
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>My HTMX App</title>
-        </head>
-        <body>
-            <h1>Hello, world!</h1>
-        </body>
-        </html>
-        """
+        content = "HELLOOOOO"
 
-        self.wfile.write(html_content.encode('utf-8'))
+        self.wfile.write(content.encode('utf-8'))
+
+        return
+    
