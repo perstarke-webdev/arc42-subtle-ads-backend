1100fa0e207bf3e60a86a9c756556300a3d0c6bf
Per Starke <per@starke-team.de>
2023-12-10 18:07:28 +0100

    try handling options request

--- api/index.py
+++ api/index.py
@@ -10,8 +10,20 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('HX-Swap', 'true')
         self.end_headers()
 
-        content = "<h1>HELLOOOOO TEST2</h1>"
+        content = "<h1>HELLOOOOO</h1>"
 
         self.wfile.write(content.encode('utf-8'))
 
-        return
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
+
+if __name__ == '__main__':
+    from http.server import HTTPServer
+    server = HTTPServer(('0.0.0.0', 8000), handler)
+    print('Starting server on port 8000...')
+    server.serve_forever()
+
