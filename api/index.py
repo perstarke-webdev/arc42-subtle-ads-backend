7ef708bd02526dd4969a0196d9038abe8bbc8974
Per Starke <per@starke-team.de>
2023-12-11 18:06:34 +0100

    more headers in python

--- api/index.py
+++ api/index.py
@@ -1,11 +1,24 @@
 from http.server import BaseHTTPRequestHandler
 
-
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
+        # Add headers for htmx compatibility
+        self.send_header('HX-Request', 'true')
+        self.send_header('HX-Trigger', 'true')
+        self.send_header('HX-Swap', 'true')
+        # Add CORS headers for the specific route
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        self.wfile.write('<h1>Helloooo Test World</h1>'.encode('utf-8'))
-        return
+
+        content = "<h1>Helloooo Test World 1</h1>"
+        self.wfile.write(content.encode('utf-8'))
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
