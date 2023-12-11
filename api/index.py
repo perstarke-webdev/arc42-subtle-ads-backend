73c28c0ecc70227cd5e270f5cc075dcc38ebd901
Per Starke <per@starke-team.de>
2023-12-11 18:02:23 +0100

    content-type html

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write('<h1>Hello, world!</h1>'.encode('utf-8'))
+        self.wfile.write('<h1>Helloooo Test World</h1>'.encode('utf-8'))
         return
