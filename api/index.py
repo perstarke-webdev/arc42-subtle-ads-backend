515afbcb3589621444d5024431e5e42d7e49c37d
Per Starke <per@starke-team.de>
2023-12-12 15:16:55 +0100

    add do_options

--- api/index.py
+++ api/index.py
@@ -1,24 +1,22 @@
 from http.server import BaseHTTPRequestHandler
 
-
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
-        if self.path.startswith("/api"):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.send_header('Access-Control-Allow-Credentials', 'true')
-            self.send_header('Access-Control-Allow-Origin', '*')
-            self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT')
-            self.send_header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
-            self.end_headers()
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        # Add headers for htmx compatibility
+        self.send_header('HX-Request', 'true')
+        self.send_header('HX-Trigger', 'true')
+        self.send_header('HX-Swap', 'true')
+        self.end_headers()
 
-            content = "<h1>Test</h1>"
+        content = "<h1>HELLOOOOO</h1>"
 
-            self.wfile.write(content.encode('utf-8'))
-        else:
-            self.send_response(404)
-            self.end_headers()
-            self.wfile.write(b'Not Found')
+        self.wfile.write(content.encode('utf-8'))
 
-        return
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
