1d163ce792950fc14a794470e43822cac3e3ee2e
Per Starke <per@starke-team.de>
2023-12-12 15:12:52 +0100

    more explicit headers and full html structure

--- api/index.py
+++ api/index.py
@@ -4,8 +4,33 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write('<div>Test1</div>'.encode('utf-8'))
+        if self.path.startswith("/api"):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.send_header('Access-Control-Allow-Credentials', 'true')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT')
+            self.send_header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+            self.end_headers()
+
+            content = """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Test Page</title>
+            </head>
+            <body>
+                <div>Test1</div>
+            </body>
+            </html>
+            """
+
+            self.wfile.write(content.encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Not Found')
+
         return
