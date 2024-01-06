75c11b19312c2c5d60d7c2354b9070cd5e794d85
Per Starke <per@starke-team.de>
2024-01-06 17:51:28 +0100

    try more flask / cors stuff

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-
-@app.route("/")
+@app.route("/", methods=["GET", "OPTIONS"])
 def hello_world():
+    if request.method == "OPTIONS":
+        response = app.make_default_options_response()
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        return response
+
     return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
